b21899e9d978e2ad1f8b87f48e5c34f4104300b0
tracer-ics2017 <tracer@njuics.org>
2026-05-17 02:40:09 +0000

    >  compile 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  02:40:09 up 1 day, 16:33,  0 users,  load average: 0.16, 0.04, 0.07 5c91ea0ca135ee12a37ab57ea9b5c5cc4c916047

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -23,14 +23,50 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  //for (i = 0; i < _screen.width * _screen.height; i++) {
+    //fb[i] = i;
+  //}
+  
+  for (i = 0; i < h; i++) {
+    int yy = y + i;
+    
+    if (yy < 0 || yy >= _screen.height) {
+      continue;
+    }
+
+    int xx = x;
+    int ww = w;
+    const uint32_t *src = pixels + i * w;
+
+    if (xx < 0) {
+      src += -xx;
+      ww += xx;
+      xx = 0;
+    }
+
+    if (xx + ww > _screen.width) {
+      ww = _screen.width - xx;
+    }
+
+    if (ww <= 0) {
+      continue;
+    }
+
+    memcpy(&fb[yy * _screen.width + xx], src, ww * sizeof(uint32_t));
   }
 }
 
 void _draw_sync() {
 }
 
+#define I8042_DATA_PORT 0x60
+#define I8042_STATUS_PORT 0x64
+#define I8042_STATUS_HASKEY_MASK 0x1
+
 int _read_key() {
-  return _KEY_NONE;
+  if ((inb(I8042_STATUS_PORT) & I8042_STATUS_HASKEY_MASK) == 0) {
+    return _KEY_NONE;
+  }
+
+  return inl(I8042_DATA_PORT);
 }
