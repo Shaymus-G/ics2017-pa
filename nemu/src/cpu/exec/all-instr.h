b168f3cf520abd6868522c791f15f40034b2f704
tracer-ics2017 <tracer@njuics.org>
2026-05-17 01:46:59 +0000

    >  compile 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  01:46:59 up 1 day, 15:40,  0 users,  load average: 0.08, 0.10, 0.07 85c8466a4b461c257a4346cf2287b7beb2a6b54

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -16,6 +16,7 @@ make_EHelper(sbb);
 make_EHelper(cmp);
 make_EHelper(inc);
 make_EHelper(dec);
+make_EHelper(neg);
 make_EHelper(mul);
 make_EHelper(imul1);
 make_EHelper(imul2);
--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -125,7 +125,19 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  TODO();
+  rtl_sub(&t2, &tzero, &id_dest->val);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_neq0(&t0, &id_dest->val);
+  rtl_set_CF(&t0);
+
+  rtl_msb(&t0, &id_dest->val, id_dest->width);
+  rtl_msb(&t1, &t2, id_dest->width);
+  rtl_and(&t0, &t0, &t1);
+  rtl_set_OF(&t0);
+
+  operand_write(id_dest, &t2);
 
   print_asm_template1(neg);
 }
