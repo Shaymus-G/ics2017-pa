ae03332b57dd98b2051301293aa2120b49c4cabd
tracer-ics2017 <tracer@njuics.org>
2026-05-17 04:38:15 +0000

    >  compile 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  04:38:15 up 1 day, 18:31,  0 users,  load average: 0.08, 0.03, 0.01 5d3d949f7a2fb1b997e748ff3271ec672230b05d

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -158,7 +158,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   rtl_sm(&cpu.esp, 4, src1);
 }
 
-static inline void rtl_pop(rtlreg_t* dest) {
+void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
   rtl_lm(dest, &cpu.esp, 4);
