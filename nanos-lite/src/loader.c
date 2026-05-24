0fd950f42a07f22ac78106e5e5900a50a7fd0d81
tracer-ics2017 <tracer@njuics.org>
2026-05-24 03:09:39 +0000

    >  run 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  03:09:39 up  8:49,  0 users,  load average: 0.07, 0.04, 0.01 4d8c9b0618835250323de3df3ec511bf511033ae

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,17 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern size_t get_ramdisk_size();
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  (void)as;
+
+  size_t size = get_ramdisk_size();
+
+  ramdisk_read(DEFAULT_ENTRY, 0, size);
+
+  Log("load program from ramdisk: filename = %s, size = %d, entry = %p", filename, size, DEFAULT_ENTRY);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
