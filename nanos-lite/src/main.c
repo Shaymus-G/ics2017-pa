0c153c0fbcbafd78c557244627d6e8ab18f091d0
tracer-ics2017 <tracer@njuics.org>
2026-05-24 12:07:55 +0000

    >  run 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  12:07:55 up 14:52,  0 users,  load average: 0.07, 0.11, 0.34 40c9e34afad56fc6464e8032ee36919c28d03eb9

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -4,6 +4,43 @@
 #define HAS_ASYE
 //#define HAS_PTE
 
+#define USER_STACK_TOP 0x08000000
+
+static void enter_user(uint32_t entry, const char *filename) {
+  uintptr_t sp = USER_STACK_TOP;
+
+  size_t len = strlen(filename) + 1;
+  sp -= len;
+  memcpy((void *)sp, filename, len);
+  uintptr_t argv0 = sp;
+
+  sp &= ~0x3;
+
+  sp -= 4;
+  *(uintptr_t *)sp = 0;
+
+  sp -= 4;
+  *(uintptr_t *)sp = 0;
+
+  sp -= 4;
+  *(uintptr_t *)sp = argv0;
+
+  sp -= 4;
+  *(uintptr_t *)sp = 1;
+
+  Log("enter user: entry = 0x%x, esp = 0x%x, argv0 = %s", entry, sp, (char *)argv0);
+
+  asm volatile(
+    "movl %0, %%esp;"
+    "jmp *%1"
+    :
+    : "r"(sp), "r"(entry)
+    : "memory"
+  );
+
+  panic("Should not reach here");
+}
+
 void init_mm(void);
 void init_ramdisk(void);
 void init_device(void);
@@ -30,8 +67,11 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/init");
-  ((void (*)(void))entry)();
+  //uint32_t entry = loader(NULL, "/bin/init");
+  //((void (*)(void))entry)();
+  const char *filename = "/bin/init";
+  uint32_t entry = loader(NULL, filename);
+  enter_user(entry, filename);
 
   panic("Should not reach here");
 }
