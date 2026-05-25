6e828fee664e62c291656eead93f4ca9bd397353
tracer-ics2017 <tracer@njuics.org>
2026-05-25 04:52:16 +0000

    >  run 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  04:52:16 up 20:31,  0 users,  load average: 0.38, 0.26, 0.14 3fd4a8d93729e2d37c8882419a83d9057f2e102

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,7 +13,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
   (void)as;
 
   if (filename == NULL) {
-    filename = "/bin/init";
+    filename = "/bin/pal";
   }
 
   int fd = fs_open(filename, 0, 0);
