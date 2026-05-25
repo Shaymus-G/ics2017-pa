de6718ee71df3b2068c8f2339c5f2fed5b571551
tracer-ics2017 <tracer@njuics.org>
2026-05-25 07:53:16 +0000

    >  run 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  07:53:16 up 22:18,  0 users,  load average: 0.01, 0.04, 0.05 54c0d027d035c6535238dbe457e8afa296ef66e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -14,7 +14,6 @@ size_t events_read(void *buf, size_t len) {
   static char event_buf[32];
   static size_t event_len = 0;
   static size_t event_pos = 0;
-  static unsigned long last_time = 0;
 
   if (len == 0) {
     return 0;
@@ -37,80 +36,30 @@ size_t events_read(void *buf, size_t len) {
     return n;
   }
 
-  while(1) {
-    int key = _read_key();
+  int key = _read_key();
 
-    if (key != _KEY_NONE) {
-      //const char *type;
-      //int keycode;
+  if (key != _KEY_NONE) {
+    const char *type;
+    int keycode;
 
-      //if (key & 0x8000) {
-        //type = "kd";
-	//keycode = key & ~0x8000;
-      //} else {
-	//type = "ku";
-	//keycode = key;
-      //}
-
-      //if (keycode > 0 && keycode < 256 && keyname[keycode] != NULL) {
-	//event_len = snprintf(event_buf, sizeof(event_buf), "%s %s\n", type, keyname[keycode]);
-
-	//Log("events_read return event: %s", event_buf);
-	//break;
-      //}
-      return 0;
+    if (key & 0x8000) {
+      type = "kd";
+      keycode = key & ~0x8000;
+    } else {
+      type = "ku";
+      keycode = key;
     }
-
-  //int key = _read_key();
-
-  //if (key != _KEY_NONE) {
-    //const char *type;
-    //int keycode;
-
-    //if (key & 0x8000) {
-      //type = "kd";
-      //keycode = key & ~0x8000;
-    //} else {
-      //type = "ku";
-      //keycode = key;
-    //}
 
     //Log("events_read: raw_key=0x%x, type=%s, keycode=%d, keyname=%s", key, type, keycode, (keycode > 0 && keycode <256 && keyname[keycode] != NULL) ? keyname[keycode] : "UNKNOWN");
 
-    //if (keycode > 0 && keycode < 256 && keyname[keycode] != NULL) {
-      //event_len = snprintf(event_buf, sizeof(event_buf), "%s %s\n", type, keyname[keycode]);
+    if (keycode > 0 && keycode < 256 && keyname[keycode] != NULL) {
+      event_len = snprintf(event_buf, sizeof(event_buf), "%s %s\n", type, keyname[keycode]);
       //Log("events_read return event: %s", event_buf);
-    //} else {
-      //event_len = snprintf(event_buf, sizeof(event_buf), "t %u\n", (unsigned)_uptime());
-    //}
-  //} else {
-    //event_len = snprintf(event_buf, sizeof(event_buf),  "t %u\n", (unsigned)_uptime());
-  //}
-
-  //event_pos = 0;
-
-  //size_t n = event_len;
-  //if (n > len) {
-    //n = len;
-  //}
-
-  //memcpy(buf, event_buf, n);
-  //event_pos += n;
-
-  //if (event_pos >= event_len) {
-    //event_pos = 0;
-    //event_len = 0;
-  //}
-
-  //return n;
-//}
-
-    unsigned long now = _uptime();
-    if (now - last_time >= 100) {
-      last_time = now;
-      event_len = snprintf(event_buf, sizeof(event_buf), "t %d\n", (int)now);
-      break;
+    } else {
+      event_len = snprintf(event_buf, sizeof(event_buf), "t %d\n", (int)_uptime());
     }
+  } else {
+    event_len = snprintf(event_buf, sizeof(event_buf), "t %d\n", (int)_uptime());
   }
 
   event_pos = 0;
@@ -122,7 +71,7 @@ size_t events_read(void *buf, size_t len) {
 
   memcpy(buf, event_buf, n);
   event_pos += n;
-    
+
   if (event_pos >= event_len) {
     event_pos = 0;
     event_len = 0;
@@ -130,11 +79,6 @@ size_t events_read(void *buf, size_t len) {
 
   return n;
 }
-
-  //return 0;
-  //int n =snprintf((char *)buf, len, "t %lu\n", _uptime());
-  //return n < len ? n : len;
-//}
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
   memcpy(buf, dispinfo + offset, len);
