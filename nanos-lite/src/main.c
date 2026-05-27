d49eb703334ac2a1f602309873a1aa9f8094dd5c
tracer-ics2017 <tracer@njuics.org>
2026-05-27 06:44:34 +0000

    >  run 2412082 gjh Linux 0f8d13b1d113 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  06:44:34 up  2:42,  0 users,  load average: 0.26, 0.52, 0.48 d65aac05cd5a12ef20e4e1be75e669ab8462652

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -80,7 +80,7 @@ int main() {
 
   //uint32_t entry = loader(NULL, "/bin/init");
   //((void (*)(void))entry)();
-  const char *filename = "/bin/pal";
+  const char *filename = "/bin/hello";
   uint32_t entry = loader(NULL, filename);
   enter_user(entry, filename);
 
